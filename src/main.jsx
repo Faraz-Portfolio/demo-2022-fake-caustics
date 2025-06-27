a7b446d0f26b733b04ea1fe9c8173286d0ff2d46
Faraz Shaikh <farazzshaikh@gmail.com>
2025-06-27 16:17:02 +0400

    Update submodule demo-2022-fake-caustics

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { UI } from "./components/ui.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <>
     <App />
-    <UI />
-  </StrictMode>
+  </>
 );
